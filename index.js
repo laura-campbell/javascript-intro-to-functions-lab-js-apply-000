44162543ee57b8ceaea7769e175bc0fd20fba721
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-07 08:01:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()){
+  if (string === 'I love you, Grandma.') {
+  return "I love you, too."
+  }
+  else if (string === string.toLowerCase()){
   return "I can't hear you!"}
   else if (string === string.toUpperCase()){
     return "YES INDEED!"}
