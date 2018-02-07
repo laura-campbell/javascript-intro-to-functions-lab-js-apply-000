d262c1881983c753f0d852f6ed4092d78fce060d
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-07 07:54:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
 }
